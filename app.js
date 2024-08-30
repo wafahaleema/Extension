8dcc9b380e1127bea55ebee0afda7ece97f76481
wafahaleema <156582894+wafahaleema@users.noreply.github.com>
2024-08-30 22:18:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const getjokes = async () => {
+    try{
+        const res = await fetch("https://api.chucknorris.io/jokes/random");
+        const data = await res.json();
+        const myjoke =document.querySelector("#myjoke");
+        myjoke.innerHTML = data.value;
+    } catch(error) {}
+};
+
+window.addEventListener("load",() => {
+    getjokes();
+});
